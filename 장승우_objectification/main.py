b91978af4746fffe4ccbef600a36f92402b62c86
HillOfMay <35472882+HillOfMay@users.noreply.github.com>
2022-06-17 11:29:50 +0900

    그래프 줄력까지 추가한 수정본입니다.

--- 장승우_objectification/main.py
+++ 장승우_objectification/main.py
@@ -1,5 +1,5 @@
-import EDA
-import dPP
+import Preprocessing
+import Engineering
 import Modeling
 import pandas as pd
 
@@ -7,13 +7,17 @@ def main() :
     print("Data loading...")
     train = pd.read_csv("./data/train_V2.csv")
     test = pd.read_csv("./data/test_V2.csv")
-    print("Data loaded!")
+    print("Data loaded!\n")
     
-    train = dPP.dropOutlier(train)
-    train = dPP.encodeMatch(train)
-    train = dPP.makeCols(train)
+    Preprocessing.checkNaN(train)
+    train = Preprocessing.dropNaN(train)
     
-    train = Modeling.Modeling_LGBM(train)
+    train = Engineering.dropOutlier(train)
+    train = Engineering.encodeMatch(train)
+    train = Engineering.makeCols(train)
+    
+    train = Modeling.fit_model(train)
 
 if __name__=="__main__" :
     main()
+
